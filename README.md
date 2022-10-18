42a3aae70d6e49284cdd9c646fa657fede9748ff
Rubén Saborido <rsain@uma.es>
2022-10-18 17:06:14 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# AdsBannerTemplate
+# ChartboostAdsBanner
 
 This is a minimal application, only featuring an activity. It is meant to be
-cloned to study energy consumption of advertising third-party libraries.
+cloned to study Chartboost SDK performance.
 
 ## Scenario
 
 The `scenario.sh` script allows you to automatically run the application.
 
-It will automatically launch the app and wait a defined amount of time to load ads.
+It will automatically launch the app and wait a defined amount of time to load ads. NOTE that it is not possible to call API show method directly after cache method for the same location, or the SDK will fail silently. This is why the minimal app has two buttons: one to cache the ad and another one to show it. Therefore, the scenario must tap these two buttons in order to load ads.
 
 To use it, make sure you have `adb` installed and an Android device with dev mode plugged-in.
 
---
+++ app/src/main/java/tpl/ads/banner/chartboost/MainActivity.java
@@ -0,0 +1,98 @@
+package tpl.ads.banner.chartboost;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.chartboost.sdk.Chartboost;
+import com.chartboost.sdk.LoggingLevel;
+import com.chartboost.sdk.ads.Banner;
+import com.chartboost.sdk.callbacks.BannerCallback;
+import com.chartboost.sdk.callbacks.StartCallback;
+import com.chartboost.sdk.events.CacheError;
+import com.chartboost.sdk.events.CacheEvent;
+import com.chartboost.sdk.events.ClickError;
+import com.chartboost.sdk.events.ClickEvent;
+import com.chartboost.sdk.events.ImpressionEvent;
+import com.chartboost.sdk.events.ShowError;
+import com.chartboost.sdk.events.ShowEvent;
+import com.chartboost.sdk.events.StartError;
+
+public class MainActivity extends AppCompatActivity  {
+    Banner chartboostBanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Chartboost.startWithAppId(getApplicationContext(), getResources().getString(R.string.appId), getResources().getString(R.string.appSignature), new StartCallback() {
+            @Override
+            public void onStartCompleted(@Nullable StartError startError) {
+                if (startError != null) {
+                    Log.println(Log.INFO, "INFO", "*** MY MESSAGE ***: NOT STARTED");
+                    Log.println(Log.INFO,"INFO",startError.toString());
+                }
+            }
+        });
+
+        chartboostBanner = new Banner(this.getApplicationContext(), "location", Banner.BannerSize.STANDARD, new BannerCallback() {
+            @Override
+            public void onAdLoaded(@NonNull CacheEvent cacheEvent, @Nullable CacheError cacheError) {
+            }
+
+            @Override
+            public void onAdRequestedToShow(@NonNull ShowEvent showEvent) {
+            }
+
+            @Override
+            public void onAdShown(@NonNull ShowEvent showEvent, @Nullable ShowError showError) {
+            }
+
+            @Override
+            public void onAdClicked(@NonNull ClickEvent clickEvent, @Nullable ClickError clickError) {
+            }
+
+            @Override
+            public void onImpressionRecorded(@NonNull ImpressionEvent impressionEvent) {
+            }
+        }, null);
+
+        setContentView(R.layout.activity_main);
+
+        // Needs to be set before SDK init
+        //Chartboost.addDataUseConsent(getApplicationContext(), new GDPR(GDPR.GDPR_CONSENT.BEHAVIORAL));
+        //Chartboost.addDataUseConsent(getApplicationContext(), new CCPA(CCPA.CCPA_CONSENT.OPT_IN_SALE));
+        Chartboost.setLoggingLevel(LoggingLevel.ALL);
+
+        Button cacheButton = findViewById(R.id.buttonCache);
+        Button showButton = findViewById(R.id.buttonShow);
+        RelativeLayout bannerHolder = findViewById(R.id.example_banner_holder);
+
+        cacheButton.setOnClickListener(v -> cacheBanner());
+        showButton.setOnClickListener(v -> showBanner());
+
+        //Attach object to layout
+        bannerHolder.addView(chartboostBanner);
+    }
+
+    private void showBanner() {
+        chartboostBanner.show();
+    }
+
+    private void cacheBanner() {
+        chartboostBanner.cache();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(chartboostBanner != null) {
+            chartboostBanner.detach();
+        }
+    }
+}
